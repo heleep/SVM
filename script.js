330daadc0217efb823ce3bfb26262f90e6e685de
Patel Prince <patelprince2635@gmail.com>
2025-03-30 17:06:24 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Slider functionality
 const slides = [
   {
-    image: 'https://hd.wallpaperswide.com/thumbs/most_beautiful_mountain_scenery-t2.jpg',
+    image: 'https://wallpapercave.com/wp/xl8hSG3.jpg',
     title: 'Welcome to SVM Institute of Technology',
     description: 'Empowering future innovators with excellence in education.'
   },
